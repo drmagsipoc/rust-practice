1f269286da7f1e3d60049b980a67dfb18b9fb1f0
p_2 <30770254+drmagsipoc@users.noreply.github.com>
2017-08-26 17:55:58 +0800

    added generic find largest

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,24 @@
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main()
 {
-    println!("Hello, world!");
+    let numbers = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&numbers);
+    println!("The largest number is {}", result);
+
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&numbers);
+    println!("The largest number is {}", result);
 }
