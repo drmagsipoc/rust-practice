160782d63cd8913fcc0e74b08b66c1880ee0f28a
p_2 <30770254+drmagsipoc@users.noreply.github.com>
2017-08-26 22:35:57 +0800

    added largest_char

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,16 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -14,11 +26,11 @@ fn main()
 {
     let numbers = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&numbers);
+    let result = largest_i32(&numbers);
     println!("The largest number is {}", result);
 
-    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let chars = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&numbers);
+    let result = largest_char(&chars);
     println!("The largest number is {}", result);
 }
